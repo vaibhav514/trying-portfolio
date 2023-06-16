769bf8e78f6737242453b75037de477caaeb7587
Vaibhav Agrawal <90774825+vaibhav514@users.noreply.github.com>
2023-06-16 16:01:59 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import water from "../../Assets/Projects/water.png";
+import photo from "../../Assets/Projects/photo.png";
 
 function Projects() {
   return (
